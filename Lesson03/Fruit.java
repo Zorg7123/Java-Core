1294c7ea432b515d6982869b73091f08253b5f25
Zorg7123 <89041322+Zorg7123@users.noreply.github.com>
2021-09-28 14:05:56 +0300

    Lesson3

---
+++ Lesson03/Fruit.java
@@ -0,0 +1,13 @@
+package Lesson03;
+
+public class Fruit {
+    protected float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
