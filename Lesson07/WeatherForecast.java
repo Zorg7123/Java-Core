de8370aecb706c6eaa2b07a76b306f837009fb2d
Zorg7123 <89041322+Zorg7123@users.noreply.github.com>
2021-10-19 00:51:36 +0300

    81

---
+++ Lesson07/WeatherForecast.java
@@ -0,0 +1,10 @@
+package Lesson07;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+
+public class WeatherForecast {
+    public void getWeatherForecast(int option, String cityID) throws IOException, ParseException, ClassNotFoundException, SQLException;
+    public void printDataFromDB() throws ClassNotFoundException, SQLException;
+}
